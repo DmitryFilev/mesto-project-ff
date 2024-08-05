2cee49e6cd3802eb43c189c17bacb9907b4e290f
Dmitry Filev <dmitryf41@yandex.ru>
2024-08-05 09:19:14 +0300

    version 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
+
 // @todo: Темплейт карточки
-
+const cardTmp = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardsContainer = document.querySelector('.places__list');
+// @todo: Вывести карточки на страницу 
+initialCards.forEach ( element => cardsContainer.append(addCard(element.name, element.link, delCard)));
 // @todo: Функция создания карточки
-
+function addCard (nameCard, linkCard, funcDelCard) {
+    const cardNative = cardTmp.querySelector('.places__item').cloneNode(true);
+    cardNative.querySelector('.card__image').src = linkCard;
+    cardNative.querySelector('.card__title').textContent = nameCard;  
+    cardNative.querySelector('.card__delete-button').addEventListener('click', () => funcDelCard(cardNative));
+    return cardNative;
+}
 // @todo: Функция удаления карточки
+function delCard (cardForDel) {
+    cardForDel.remove();
+};
 
-// @todo: Вывести карточки на страницу
